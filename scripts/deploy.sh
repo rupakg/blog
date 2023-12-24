f931683ab3c8ec25be2238bd276a0c27857101f9
Rupak Ganguly <rupakg@gmail.com>
2023-12-24 11:23:42 -0500

    Update deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 # Build the project.
 hugo -t hyde-hyde
 
+echo -e "\033[0;32mAdd public folder contents...\033[0m"
 # Go To Public folder
 cd public
 # Add changes to git.
 git add -A
 
+echo -e "\033[0;32mCommit changes to public folder...\033[0m"
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
@@ -17,6 +19,7 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
+echo -e "\033[0;32mPush changes to public folder to Github...\033[0m"
 # Push source and build repos.
 git push origin master
 
