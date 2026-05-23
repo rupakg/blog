957ec1e91200f9d8c90b652b1fb7bb72b300ee30
Rupak Ganguly <rupakg@gmail.com>
2026-05-22 20:57:12 -0400

    Fix minor issue and update msg.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,19 +7,19 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 echo -e "\033[0;32mAdd public folder contents...\033[0m"
 # Go To Public folder
-cd public
+cd ../public
 # Add changes to git.
 git add -A
 
 echo -e "\033[0;32mCommit changes to public folder...\033[0m"
 # Commit changes.
-msg="rebuilding site `date`"
+msg="Rebuilding the site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-echo -e "\033[0;32mPush changes to public folder to Github...\033[0m"
+echo -e "\033[0;32mPush changes to public folder on Github...\033[0m"
 # Push source and build repos.
 git push origin master
 
